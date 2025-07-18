19f818e8cd657f8301f5ee82b4b8b2e668b1c65a
vidaljoao <vidaljoao1994@gmail.com>
2025-07-17 21:26:58 -0300

    Add files via upload

---
+++ pg.py
@@ -0,0 +1,81 @@
+#print('meu nome é ', 'pynthon.', sep='- ')
+
+
+#print('meu', 'nome', 'é', sep='_', end='*')
+
+
+#print('monty', 'pinton.', sep='*', end='*\n')
+
+
+#print('programção', 'essencial', 'em python',sep='***', end='...')
+#print(   '    *', '   *  *', '  * *' , '  *     *',' ***   ***',  '  *    *','  *    *', '  ******', sep='\n')
+#print(3e8)
+
+
+
+#print("eu gosto 'monty python'")
+#print('"Eu Sou"','""aprendizado""','""""python"""', sep='\n')
+#print(0b1011)
+#print(2 ** 3)
+#print(2 ** 3.)
+#print(2 * 3)
+#print(6. / 3)
+#print(6 // 4.)
+#print(-6 // 4.)
+#print(10/0)
+#print(-4 + 4)
+#print(-4. - 8)
+#print(-4. - 8)
+#print(-4. - 8)
+#print(25%13)
+#print((2**4),(2*4.),(2*4))
+#print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+#print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+#var=1
+#print(var)
+#var = 1
+#saldo_da_conta = 1000.0
+#nome_cliente = 'John Doe'
+#print(var, saldo_da_conta, nome_cliente)
+#print(var)
+
+#var = 1
+#print(var)
+#var = var + 5
+#print(var)
+
+#pes=82
+#alt=1.8
+#print(pes/alt**2)
+#print(82/1.8**2)
+#John = 3
+#Maria = 5
+#Adam = 6
+#j='jhon '
+#m=', maria '
+#ad=', adam '
+#fruta = ' maçãs '
+#total_macas= John + Maria + Adam
+#print(j,John,fruta,m,Maria,fruta,ad,Adam,fruta)
+#print('Total de maçãs: ', total_macas)
+#k=6.000
+#m=0.6214
+#print(k*m)
+#milhas= 0.6214
+#minha_casa=6
+#resultado= minha_casa*milhas
+#print('até minha casa são', resultado,'milha')
+
+
+#Converter 1 hora em minutos
+#uma_hora= 60 #levando em consideração que 1 hora é 60 minutos.
+#um_minuto= 60 # 1 minuto é igual 60 segundos
+
+#print(uma_hora*um_minuto*3)
+
+
+
+idade = int(input("qual sua idade? "))
+# idade=int(idade)
+print("A idade fornecida foi", idade)
+print("você nasceu em", 2025-idade)
