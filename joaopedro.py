072e17f7409e7dc6776179fc1d89e4c33ce6a5a1
vidaljoao <vidaljoao1994@gmail.com>
2025-07-17 21:30:01 -0300

    lista de alunos completa

--- joaopedro.py
+++ joaopedro.py
@@ -2,7 +2,7 @@ nome='João Pedro'
 local = 'Senac - Parnaiba'
 print(f'Nome: {nome} está no {local}')
 
-alunos=['faustino','douglas','daniel','samuel','john','flavio']
+alunos=['faustino','douglas','daniel','samuel','john','flavio', 'charles', 'lucas', 'francisco', 'fabiano', 'nataly', 'pedro', 'vitor', 'samara']
 
 alunos.sort()
 
