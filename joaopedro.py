42102284a109404b894b7693cd0967eb0d6526a8
vidaljoao <vidaljoao1994@gmail.com>
2025-07-17 21:22:30 -0300

    first commit

---
+++ joaopedro.py
@@ -0,0 +1,10 @@
+nome='João Pedro'
+local = 'Senac - Parnaiba'
+print(f'Nome: {nome} está no {local}')
+
+alunos=['faustino','douglas','daniel','samuel','john','flavio']
+
+alunos.sort()
+
+for aluno in alunos:
+    print(f'nome: {aluno}')
